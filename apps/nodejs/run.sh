1961dcc793bc8a5bb5a5d49145b875e7bd736add
yurii.furko <yurii.furko@gmail.com>
2024-03-13 00:01:33 +0200

    configure jaeger

--- apps/nodejs/run.sh
+++ apps/nodejs/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it -v $PWD:/home/node/app --net=host -p 3000:3000 $(docker build -q .) sh
+docker run --rm -it -v $PWD:/home/node/app -p "3000:3000" $(docker build -q .) sh
